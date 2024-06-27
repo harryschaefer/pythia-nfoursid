1d2dcda82ad3d6c9200fea9943ecc4af994e5049
harryschaefer <harryschaefer@outlook.com>
2024-06-27 21:03:42 +1000

    Initialise IBR model, requirements

---
+++ notebook/__init__.py
@@ -0,0 +1,5 @@
+
+from .inter.nfoursid_e import NFourSID_e
+from .inter.state_space_e import StateSpace_e
+
+from .sim_settings import SimSettings
--- notebook/sim_settings.py
+++ notebook/sim_settings.py
@@ -1,6 +1,8 @@
 
 # select the model to fit
-import models.provided as model
+from .models import provided
+from .models import ibr
+
 
 class SimSettings():
 
@@ -15,17 +17,25 @@ class SimSettings():
     FIGSIZE = 3
     figsize = (FIGSIZE*1.5, FIGSIZE)
 
-    TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjNkNTgwZjBhZjdhY2U2OThhMGNlZTdmMjMwYmNhNTk0ZGM2ZGJiNTUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA5ODQwNzExNjMyMzQ0NzI4NTc5IiwiaGQiOiJnZW5lcmFsLWRldi5jbyIsImVtYWlsIjoiaGFycnkuc2NoYWVmZXJAZ2VuZXJhbC1kZXYuY28iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6Ind2WTRBQWtKNkNaOEt4UzdhSDd0a0EiLCJpYXQiOjE3MTk0MDk0NDMsImV4cCI6MTcxOTQxMzA0M30.mQVhkBpOCtGPixUQ0W5eu6Ym5kN5YuBAdqXi2D8yYVPfiJvr_uvdKeJ6xXlCRwPj53i5HA_b_e7eO_zCaog9GlZbBkI4ZCAuLG3m17pGWsiSGU3C3ZjWqiaVwwTcUY3YZV6yo1vOEy-shx9Gk3OU8PolpfLvALn5nmymGxDAhm-QB5iwvNE1rX9ZloKk4iBb0gpudAwfMPq6jqAHJtg_Hbm6hicw9_C06mhnQbGK0uPSRhjirIhrwagqTycT_4OjK0qMYnjoh_jko7mYk2GscrhaOKaGgalfRtLFSSSsM_vsCJLo9pRmoXZbZWhXp-gzcd5UmvxeZ9UTCE8zWHnA_A"
+    TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjJhZjkwZTg3YmUxNDBjMjAwMzg4OThhNmVmYTExMjgzZGFiNjAzMWQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA5ODQwNzExNjMyMzQ0NzI4NTc5IiwiaGQiOiJnZW5lcmFsLWRldi5jbyIsImVtYWlsIjoiaGFycnkuc2NoYWVmZXJAZ2VuZXJhbC1kZXYuY28iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IktJMXctOURnOWY1Nmd1eWpxYXNHYWciLCJpYXQiOjE3MTk0ODE0NTYsImV4cCI6MTcxOTQ4NTA1Nn0.dLIW5m5wE0RX3b7z5uWzAk4vqS8aZj5VrDed3Ega3thU27UVEiA9bN2cizcacy4pXiaHrY5xP9RmdOBHPH6kzDwrAsYfU5bCWOTGigpRU4i0FyhgR5-C33yorKT2at_qFlX3NeOU6Pb3mZwvzpkPGQhSfsUsjEYeTLL4_NwGToOflTpFI1jyjyH964sipBxlzc2-BAd7iI3iGVwoZKKNOAYmPlOnaaxm54raxNzJiMp784UJUJewNwK37ABr3crODhzwz4PHf0kyYxhQd2-dKVCSitL0A5CUHIWJxzg01botkNx0jrZEyIbhoXm2Lyd7r7LLUV1xGkbfKHIy06bE4w"
 
-    INPUT_DIM = model.INPUT_DIM
-    OUTPUT_DIM = model.OUTPUT_DIM
-    INTERNAL_STATE_DIM = model.INTERNAL_STATE_DIM
-    A = model.A
-    B = model.B
-    C = model.C
-    D = model.D
+    INPUT_DIM = provided.INPUT_DIM
+    OUTPUT_DIM = provided.OUTPUT_DIM
+    INTERNAL_STATE_DIM = provided.INTERNAL_STATE_DIM
+    A = provided.A
+    B = provided.B
+    C = provided.C
+    D = provided.D
+    sp_A = provided.sp_A
+    sp_B = provided.sp_B
+    sp_C = provided.sp_C
+    sp_D = provided.sp_D
 
-    sp_A = model.sp_A
-    sp_B = model.sp_B
-    sp_C = model.sp_C
-    sp_D = model.sp_D
+    IBR_INPUT_DIM = ibr.INPUT_DIM
+    IBR_OUTPUT_DIM = ibr.OUTPUT_DIM
+    IBR_INTERNAL_STATE_DIM = ibr.INTERNAL_STATE_DIM
+    IBR_A = ibr.A
+    IBR_B = ibr.B
+    IBR_C = ibr.C
+    IBR_D = ibr.D
+    
